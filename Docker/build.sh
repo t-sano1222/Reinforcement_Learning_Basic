821e2ecc5965de93e6fb81be16c24c9d922a9e70
Taiga Sano <t.sano@rlg.sys.es.osaka-u.ac.jp>
2020-10-11 19:42:11 +0900

    [fix] Dockerfile
    kubeflowに対応

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 set -x
-IMAGE_NAME="rl_basic"
-IMAGE_TAG="v0.1.0"
+IMAGE_NAME="zaku.sys.es.osaka-u.ac.jp:10081/t.sano/rl_study"
+IMAGE_TAG="v0.2.0" # original-env with kubeflow
 cd ..
 docker build -t ${IMAGE_NAME}:${IMAGE_TAG} -f Docker/Dockerfile .
